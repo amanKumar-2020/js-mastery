6f791574491b2b391c0f9a5e9bbfcb46118e2648
Aman kumar <jnvaman4@gmail.com>
2025-11-10 12:26:44 +0530

    Add to card challenge

---
+++ Js learn everything/DOM-Mastery/mid-level/script.js
@@ -0,0 +1,9 @@
+// Shopping Cart (DOM Only — No Backend)
+// A list of product cards with “Add to Cart”.
+// A cart counter updates.
+// Clicking “Add” again increases quantity.
+// Show a mini-cart/sidebar (JS creates elements dynamically).
+
+let itemCount = 0;
+let cartItem = document.querySelector("#cart-count");
+
