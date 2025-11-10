55c8a809e3619864ef9c13fdcf2305c5be9ef306
Aman kumar <jnvaman4@gmail.com>
2025-11-10 16:01:24 +0530

    Add to card noob way only show count

--- Js learn everything/DOM-Mastery/mid-level/script.js
+++ Js learn everything/DOM-Mastery/mid-level/script.js
@@ -3,7 +3,21 @@
 // A cart counter updates.
 // Clicking “Add” again increases quantity.
 // Show a mini-cart/sidebar (JS creates elements dynamically).
+// ----------------XXX------------------------XXX-------------------XXXX------------
+// Basic soluton only show count not add item 
 
-let itemCount = 0;
-let cartItem = document.querySelector("#cart-count");
+// let itemCount = 0;
+// let cartItem = document.querySelector("#cart-count");
 
+// let cardButton = document.querySelectorAll(".add-to-cart");
+
+// function handleButtonClick() {
+//   itemCount++;
+//   cartItem.innerHTML = itemCount;
+// }
+
+// cardButton.forEach((el) => {
+//   if (el) {
+//     el.addEventListener("click", handleButtonClick);
+//   }
+// });
